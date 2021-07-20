f1f38cedd6cd7e4d8f9d064096c716490655bde8
Igor Morais <igorhenrique@outlook.com>
2021-07-20 15:59:10 -0300

    Terceiro exercício

--- db_escola.sql
+++ db_escola.sql
@@ -16,6 +16,7 @@ use db_escola;
 create table tb_estudantes(
 id bigint auto_increment,
 nome varchar(255),
+idade int,
 sexo varchar(255),
 curso varchar(255),
 turma int, 
@@ -23,14 +24,14 @@ nota int,
 primary key(id)
 );
 
-insert into tb_estudantes(nome, sexo, curso, turma, nota) values ("Igor Morais", "Masculino", "Desenvolvedor Java Fullstack Jr.", 26, 10);
-insert into tb_estudantes(nome, sexo, curso, turma, nota) values ("Aryclenes ", "Masculino", "Desenvolvedor Java Fullstack Jr.", 26, 10);
-insert into tb_estudantes(nome, sexo, curso, turma, nota) values ("Nathalia Silvestre", "Feminino", "Desenvolvedora Java Fullstack Jr.", 26, 10);
-insert into tb_estudantes(nome, sexo, curso, turma, nota) values ("Felipe Costa", "Masculino", "Desenvolvedor Java Fullstack Jr.", 26, 10);
-insert into tb_estudantes(nome, sexo, curso, turma, nota) values ("Wendel Matos", "Masculino", "Desenvolvedor Java Fullstack Jr.", 26, 4);
-insert into tb_estudantes(nome, sexo, curso, turma, nota) values ("Janaína Felicia", "Feminino", "Desenvolvedora Java Fullstack Jr.", 26, 6);
-insert into tb_estudantes(nome, sexo, curso, turma, nota) values ("Anibal de Souza", "Masculino", "Desenvolvedor Java Fullstack Jr.", 26, 5);
-insert into tb_estudantes(nome, sexo, curso, turma, nota) values ("Maju Coutinho", "Feminino", "Desenvolvedora Java Fullstack Jr.", 26, 0);
+insert into tb_estudantes(nome, idade, sexo, curso, turma, nota) values ("Igor Morais", 25, "Masculino", "Desenvolvedor Java Fullstack Jr.", 26, 10);
+insert into tb_estudantes(nome, idade, sexo, curso, turma, nota) values ("Aryclenes ", 26, "Masculino", "Desenvolvedor Java Fullstack Jr.", 26, 10);
+insert into tb_estudantes(nome, idade, sexo, curso, turma, nota) values ("Nathalia Silvestre", 22, "Feminino", "Desenvolvedora Java Fullstack Jr.", 26, 10);
+insert into tb_estudantes(nome, idade, sexo, curso, turma, nota) values ("Felipe Costa", 16,"Masculino", "Desenvolvedor Java Fullstack Jr.", 26, 10);
+insert into tb_estudantes(nome, idade, sexo, curso, turma, nota) values ("Wendel Matos", 90, "Masculino", "Desenvolvedor Java Fullstack Jr.", 26, 4);
+insert into tb_estudantes(nome, idade, sexo, curso, turma, nota) values ("Janaína Felicia", 30, "Feminino", "Desenvolvedora Java Fullstack Jr.", 26, 6);
+insert into tb_estudantes(nome, idade, sexo, curso, turma, nota) values ("Anibal de Souza", 40, "Masculino", "Desenvolvedor Java Fullstack Jr.", 26, 5);
+insert into tb_estudantes(nome, idade, sexo, curso, turma, nota) values ("Maju Coutinho", 29, "Feminino", "Desenvolvedora Java Fullstack Jr.", 26, 0);
 
 select * from tb_estudantes where nota > 7;
 select * from tb_estudantes where nota < 7;
